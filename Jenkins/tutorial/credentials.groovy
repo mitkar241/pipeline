36e8b4bc9143b4d14b57c43702497ce96f8622b9
raktimhalder241 <raktimhalder241@gmail.com>
2022-02-06 16:50:27 +0000

    Added catcherr file

--- Jenkins/tutorial/credentials.groovy
+++ Jenkins/tutorial/credentials.groovy
@@ -1,11 +1,21 @@
 #!/usr/bin/env groovy
 
+SSH_CRED = 'SSH_CRED'
+
 pipeline {
   agent any
+  /*
+  agent {
+    label "xxxxx"
+  }
+  */
+  environment {
+    PWD_CRED = getUnamePwd()
+  }
   stages {
     stage('Example Username/Password') {
       environment {
-        SERVICE_CREDS = credentials('UNAME_PWD_CRED')
+        SERVICE_CREDS = credentials("${PWD_CRED}")
       }
       steps {
         sh 'echo "Service user is $SERVICE_CREDS_USR"'
@@ -14,7 +24,7 @@ pipeline {
     }
     stage('Example SSH Username with private key') {
       environment {
-        SSH_CREDS = credentials('SSH_CRED')
+        SSH_CREDS = credentials("${SSH_CRED}")
       }
       steps {
         sh 'echo "SSH private key is located at $SSH_CREDS"'
@@ -22,4 +32,17 @@ pipeline {
       }
     }
   }
+}
+
+def getUnamePwd() {
+  return 'UNAME_PWD_CRED'
+  /*
+   def branchName = "${env.BRANCH_NAME}"
+   if (branchName == "xxxxxx") {
+      return 'some_credential_string'
+   }
+   else {
+      return 'some_other_credential_string'
+   }
+   */
 }
